2c4e9f3ccb1295941f7cdfdcb51e922ae9a65711
Number99999 <nguyenngocduong.ecorphd@gmail.com>
2025-06-03 11:21:13 +0700

    ui (without logic) Ver 1.0.0

--- app/src/main/java/com/cmzsoft/weather/activity_setting_scene.kt
+++ app/src/main/java/com/cmzsoft/weather/activity_setting_scene.kt
@@ -1,11 +1,10 @@
 package com.cmzsoft.weather
 
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
-import android.widget.Toast
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,14 +22,66 @@ class activity_setting_scene : AppCompatActivity() {
         }
 
         initEventBack()
+        setupSpinnerTemp()
+        setupSpinnerRainfall()
+        setupSpinnerVisibility()
+        setupSpinnerWindSpeed()
+        setupSpinnerAtm()
+        setupSpinnerDateForm()
     }
 
     private fun initEventBack() {
         val button = findViewById<ImageButton>(R.id.backButton);
         button.setOnClickListener {
-//            val changePage = Intent(this, MainActivity::class.java);
-//            startActivity(changePage);
             finish()
         }
+    }
+
+    private fun setupSpinnerTemp() {
+        val spinner: Spinner = findViewById(R.id.spinner_temp)
+        val items = listOf("C", "F")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun setupSpinnerRainfall() {
+        val spinner: Spinner = findViewById(R.id.spinner_rain)
+        val items = listOf("mm", "cm", "m")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun setupSpinnerVisibility() {
+        val spinner: Spinner = findViewById(R.id.spinner_visibility)
+        val items = listOf("km", "Dặm")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun setupSpinnerWindSpeed() {
+        val spinner: Spinner = findViewById(R.id.spinner_wind_speed)
+        val items = listOf("m/s", "km/h", "mph")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun setupSpinnerAtm() {
+        val spinner: Spinner = findViewById(R.id.spinner_atm)
+        val items = listOf("Pa", "hPa", "mbar", "Bar", "atm")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun setupSpinnerDateForm() {
+        val spinner: Spinner = findViewById(R.id.spinner_date_format)
+        val items = listOf("dd/MM/yyyy", "MM/dd/yyyy", "yyyy/MM/dd")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
